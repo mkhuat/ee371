139aa137df556024081b43fe04f327243f1b9cff
Kevin J. Caravaggio <caravk@uw.edu>
2017-04-11 12:30:11 -0700

    Signal tap data

--- RippleUpCounter/db/ip/sld227544a3/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ RippleUpCounter/db/ip/sld227544a3/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 5ccb644f9d160715b5f2
+//   DESIGN_HASH   : 5f0e09fa73fa479527c6
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h5ccb644f9d160715b5f2 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h5f0e09fa73fa479527c6 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
