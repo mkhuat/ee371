15914c1531f4ef33e5ab91643a255c0e6fb6f795
Nick Anderson <nja4@uw.edu>
2017-04-12 09:57:11 -0700

    Added comments to ListPrice.c and added test cases to TestCases.txt

--- ListPrice/ListPrice.c
+++ ListPrice/ListPrice.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
+// calc_mark_up accepts price and mark_up, returning marked_up price
 double calc_mark_up(double price, double mark_up)
 {
 	return price + price * (mark_up/100);
 }
 
+// calc_discount accepts price and discount, returning discounted price
 double calc_discount(double price, double discount)
 {
 	return price - price * (discount/100);
 }
 
+// calc_tax accepts price and tax, returning post-tax price
 double calc_tax(double price, double tax)
 {
 	return price + price * (tax/100);
 }
 
+// validate_input accepts manufacturer_cost, mark_up, discount, and sales_tax and validates
+// these arguments as valid within the constraints of a car cost computation.
 int validate_input(double manufacturer_cost, double mark_up, double discount, double sales_tax)
 {
 	if (manufacturer_cost < 0)
@@ -32,8 +38,14 @@ int validate_input(double manufacturer_cost, double mark_up, double discount, do
 		printf("Cars must be discounted between 0%% and 100%%, but found %.2f%% \n", discount);
 		return 0;
 	}
-	if (sales_tax < 0) {
+	if (sales_tax < 0)
+	{
 		printf("Sales tax must be at least 0%%, but found %.2f%% \n", sales_tax);
+		return 0;
+	}
+	if (isinf(manufacturer_cost) || isinf(mark_up) || isinf(discount) || isinf(sales_tax))
+	{
+		printf("An input value is too large \n");
 		return 0;
 	}
 	return 1;
@@ -41,7 +53,6 @@ int validate_input(double manufacturer_cost, double mark_up, double discount, do
 
 int main(int argc, char** argv)
 {
-
 	if (argc != 5) {
 		printf("Error: Expected 4 arguments, but found %d \n", argc - 1);
 		printf("Usage: ./listPrice manufacturer_cost mark_up discount sales_tax \n");
@@ -49,10 +60,13 @@ int main(int argc, char** argv)
 	}
 
 	double manufacturer_cost, mark_up, sales_tax, discount;
-	sscanf(argv[1], "%lf", &manufacturer_cost);
-	sscanf(argv[2], "%lf", &mark_up);
-	sscanf(argv[3], "%lf", &discount);
-	sscanf(argv[4], "%lf", &sales_tax);
+	if (!sscanf(argv[1], "%lf", &manufacturer_cost) ||
+			!sscanf(argv[2], "%lf", &mark_up) ||
+			!sscanf(argv[3], "%lf", &discount) ||
+			!sscanf(argv[4], "%lf", &sales_tax)) {
+		printf("Error: Failed to scan arguments \n");
+		return 1;
+	}
 
 	if (!validate_input(manufacturer_cost, mark_up, discount, sales_tax))
 	{
@@ -62,6 +76,13 @@ int main(int argc, char** argv)
 
 	double pre_tax = calc_discount(calc_mark_up(manufacturer_cost, mark_up), discount);
 	double final_cost = calc_tax(pre_tax, sales_tax);
+
+	if (isnan(final_cost) || isinf(final_cost))
+	{
+		printf("Error: Output overflowed floating point \n");
+		return 1;
+	}
+
 	printf("Car Price Calculator \n");
 	printf("Manufacturer Cost: $%.2f, Dealer Markup: %.2f%%, Discount: %.2f%%, Sales Tax: %.2f%% \n", manufacturer_cost, mark_up, discount, sales_tax);
 	printf("Final car cost: $%.2f \n", final_cost);
