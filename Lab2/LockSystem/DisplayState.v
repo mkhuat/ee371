cca54f60de8b1dcbda627877ee4fe99b9730cb66
Melissa Khuat <melissa@D-69-91-165-144.dhcp4.washington.edu>
2017-05-01 09:49:12 -0700

    Update display sensitivity

--- Lab2/LockSystem/DisplayState.v
+++ Lab2/LockSystem/DisplayState.v
@@ -9,7 +9,7 @@ module DisplayState(state, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 	parameter wall_out_boat = 7'b1111000;
 	parameter empty = 7'b1111111;
 	
-	always @(state)
+	always @(*)
 		begin
 		// 0. 88|88|88
 		// 1. -8|88|88 
@@ -24,100 +24,100 @@ module DisplayState(state, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 		// 10.88|8- 88
 			case(state)
 				'd0: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= wall_in;
-					HEX2 <= wall_out;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = wall_in;
+					HEX2 = wall_out;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 				'd1: begin
-					HEX5 <= boat;
-					HEX4 <= empty;
-					HEX3 <= wall_in;
-					HEX2 <= wall_out;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = boat;
+					HEX4 = empty;
+					HEX3 = wall_in;
+					HEX2 = wall_out;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 				'd2: begin
-					HEX5 <= empty;
-					HEX4 <= boat;
-					HEX3 <= wall_in;
-					HEX2 <= wall_out;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = boat;
+					HEX3 = wall_in;
+					HEX2 = wall_out;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 				'd3: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= boat;
-					HEX2 <= wall_out;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = boat;
+					HEX2 = wall_out;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 				'd4: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= wall_in_boat;
-					HEX2 <= wall_out;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = wall_in_boat;
+					HEX2 = wall_out;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 				'd5: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= wall_in;
-					HEX2 <= wall_out_boat;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = wall_in;
+					HEX2 = wall_out_boat;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 				'd6: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= wall_in;
-					HEX2 <= empty;
-					HEX1 <= boat;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = wall_in;
+					HEX2 = empty;
+					HEX1 = boat;
+					HEX0 = empty;
 				end
 				'd7: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= wall_in;
-					HEX2 <= wall_out;
-					HEX1 <= boat;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = wall_in;
+					HEX2 = wall_out;
+					HEX1 = boat;
+					HEX0 = empty;
 				end
 				'd8: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= wall_in;
-					HEX2 <= wall_out;
-					HEX1 <= empty;
-					HEX0 <= boat;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = wall_in;
+					HEX2 = wall_out;
+					HEX1 = empty;
+					HEX0 = boat;
 				end
 				'd9: begin 
-					HEX5 <= empty;
-					HEX4 <= boat;
-					HEX3 <= empty;
-					HEX2 <= wall_out;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = boat;
+					HEX3 = empty;
+					HEX2 = wall_out;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 				'd10: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= wall_in;
-					HEX2 <= boat;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = wall_in;
+					HEX2 = boat;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 				default: begin
-					HEX5 <= empty;
-					HEX4 <= empty;
-					HEX3 <= wall_in;
-					HEX2 <= wall_out;
-					HEX1 <= empty;
-					HEX0 <= empty;
+					HEX5 = empty;
+					HEX4 = empty;
+					HEX3 = wall_in;
+					HEX2 = wall_out;
+					HEX1 = empty;
+					HEX0 = empty;
 				end
 			endcase
 		
