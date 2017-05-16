dc32ac73bcf3420adac92b9ad0f704d7fc84fbaa
Nicholas J. Anderson <nja4@uw.edu>
2017-05-16 11:26:40 -0700

    lab 4 nios files

---
+++ Lab4/NIOS/Nios.v
@@ -0,0 +1,10 @@
+module Nios (clk, reset, pio);
+
+	first_nios2_system u0 (
+		.clk_clk                            (clk),                            //                         clk.clk
+		.reset_reset_n                      (reset),                      //                       reset.reset_n
+		.led_pio_external_connection_export (pio)  // led_pio_external_connection.export
+	);
+
+
+endmodule
