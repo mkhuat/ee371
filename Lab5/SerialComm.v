16dea9c336d3663d9c6be0122c2a16ffd1473664
Nick Anderson <nja4@uw.edu>
2017-05-31 17:24:26 -0700

    Moving to the big screen!

--- Lab5/SerialComm.v
+++ Lab5/SerialComm.v
@@ -21,11 +21,9 @@ module SerialComm(
 
 		// Encoding of states
 		
-		
 		// BIC states
-		START_BIC = 4'b0000, // Bit inded
-
-		// 0-based Indexing makes 
+		// 0-based Indexing makes along with "Pointing to next write"
+		START_BIC = 4'b0000,
 		END_READ_BIC = 4'b1010, // When pointing to 11th bit, we have read everything
 		END_WRITE_BIC = 4'b1000, // When pointing to the 9th bit, we have read everything
 
@@ -49,28 +47,90 @@ module SerialComm(
 	
 endmodule
 
-module Loader(clk, load, parallel_in, bic_transmit) begin
+module Loader(clk, load, parallel_in, buffer_transmit, bic_transmit) begin
+	
+	// TODO: Add as parameter
+	parameter 
+		
+		// Clock index
+		CLOCK_INDEX = 24,
+		
+		// Sample index: subtract 4 because 16x faster
+		SAMPLE_INDEX = CLOCK_INDEX - 4,
+
+		// Encoding of states
+		
+		// BIC states
+		// 0-based Indexing makes along with "Pointing to next write"
+		START_BIC = 4'b0000,
+		END_READ_BIC = 4'b1010, // When pointing to 11th bit, we have read everything
+		END_WRITE_BIC = 4'b1000, // When pointing to the 9th bit, we have read everything
+
+		
+		// BSC States
+		START_BSC = 4'b0000,
+		MIDDLE_BSC = 4'b0111, // When to sample
+		END_BSC = 4'b1111;
+
 	always@(posedge load) begin
 		// Load message if the last message can be discarded, due to already being sent
-		if bic_transmit == 
-
+		if bic_transmit == END_WRITE_BIC
+			buffer_transmit = paralell_in
 	end
 end
 
-module Transmitter(clk, buffer_transmit);
-	always@(posedge clk) begin
+module Transmitter(clk, buffer_transmit, bic_transmit, transmit_enable, serial_out);
+	
+	// TODO: Add as parameter
+	parameter 
 		
+		// Clock index
+		CLOCK_INDEX = 24,
+		
+		// Sample index: subtract 4 because 16x faster
+		SAMPLE_INDEX = CLOCK_INDEX - 4,
 
+		// Encoding of states
+		
+		// BIC states
+		// 0-based Indexing makes along with "Pointing to next write"
+		START_BIC = 4'b0000,
+		END_READ_BIC = 4'b1010, // When pointing to 11th bit, we have read everything
+		END_WRITE_BIC = 4'b1000, // When pointing to the 9th bit, we have read everything
+
+		
+		// BSC States
+		START_BSC = 4'b0000,
+		MIDDLE_BSC = 4'b0111, // When to sample
+		END_BSC = 4'b1111;
+
+
+	always@(posedge clk) begin
+		// Clock out the parallel signal while transmit_enable is high and the
+		// message has not been read out fully.
+		if (transmit_enable && bic_transmit < END_WRITE_BIC) begin
+			serial_out = buffer_transmit[bic_transmit]
+			bic_transmit = bic_transmit + 1
+		end
 	end
 endmodule
 
-module Receiver(clk, buffer_receive);
+module Receiver(clk, bic_receive, buffer_receive, start_bit_received);
 	
 	output[7:0] buffer_receive;
 
 	// When to sample
 	reg[3:0] bsc;
 
+	// TODO: When can we start reading? When do we track when "Start Bit Detect" 
+	always@(posedge clk) begin
+		// Clock in the serial signal while the signal has not been read in fully
+		if (transmit_enable && bic_transmit < END_WRITE_BIC) begin
+			serial_out = buffer_transmit[bic_transmit]
+			bic_transmit = bic_transmit + 1
+		end
+
+	end
 
 endmodule
 
